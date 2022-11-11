8c933681425aaea83dde4d23eec523f13544081c
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-11 17:04:34 -0300

    Update adminn.php

--- views/adminn.php
+++ views/adminn.php
@@ -61,13 +61,14 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Clientes</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <h6 class="collapse-header">Dados Clientes</h6>
+                        <a class="collapse-item" href="cards.html">Empresa</a>
+                        <a class="collapse-item" href="cards.html">Funcionários</a>
+                        <a class="collapse-item" href="cards.html">Equipamentos</a>
                     </div>
                 </div>
             </li>
