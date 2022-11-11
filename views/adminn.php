eb05c20abc4efb3ca9da16785598feeb16eb3e0a
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-11 16:07:45 -0300

    Update adminn.php

--- views/adminn.php
+++ views/adminn.php
@@ -326,9 +326,9 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">TheWaySistemas</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-900 small">TheWaySistemas</span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="assets\img\icon.png">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
