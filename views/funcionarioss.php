fb1578c0ba98e8ed552b62d3d799fa2999e56531
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-14 10:17:23 -0300

    Update funcionarioss.php

--- views/funcionarioss.php
+++ views/funcionarioss.php
@@ -59,7 +59,7 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
+                    <i class="fa fa-users"></i>
                     <span>Clientes</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
