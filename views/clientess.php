5125d610c9aa5fc527557b53e99ed999eeebc0d0
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-21 14:01:54 -0300

    Update clientess.php

--- views/clientess.php
+++ views/clientess.php
@@ -261,7 +261,7 @@
                                     Perfil
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="login.php" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="login.php" data-toggle="#" data-target="#">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Sair
                                 </a>
