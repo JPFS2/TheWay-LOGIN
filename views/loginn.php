58f5bfb41f1e562d837380ba383ac6fce10a755b
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-21 16:07:03 -0300

    Create loginn.php

---
+++ views/loginn.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>The Way Sistemas</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/icon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">
+
+            <h1 class="logo me-auto"><a href="https://www.thewaysistemas.com.br/"> <img src="assets/img/logoT.png"
+                        alt="Logo"></a></h1>
+                        
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="getstarted scrollto" href="index.php">Home</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header> <!-- End Header -->
+
+    <!-- ======= Login ======= -->
+    <section id="cta" class="cta">
+        <div class="d-flex justify-content-center py-3">
+            <div class="section-title align-items py-4">
+                <form>
+                    <h2 class="text-light">Login</h2>
+
+                    <div class="col-md-14">
+                        <div class="row py-1">
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Usuário" required=""
+                                autofocus="">
+                        </div>
+                        <div class="row py-1">
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Senha"
+                                required="">
+                        </div>
+                    </div>
+
+                    <div>
+                        <a class="cta-btn align-middle" href="admin.php">Entrar</a>
+                    </div>
+                    <div>
+                    
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    </main>
