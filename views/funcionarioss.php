4d971c984150d29735c74914f81face17dcc75d9
Marlon Ferreira <marlonfsantos@outlook.com.br>
2022-11-23 11:38:37 -0200

    Corrigindo titulo em Funcionarios

--- views/funcionarioss.php
+++ views/funcionarioss.php
@@ -292,7 +292,7 @@ include('verifica_login.php');
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Equipamentos</h1>
+                    <h1 class="h3 mb-2 text-gray-800">funcionarios</h1>
                     <p class="mb-4"><a target="_blank" href="https://datatables.net"></a></p>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
