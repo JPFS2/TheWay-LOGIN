27edf07ad8847f41d0f87b8d30e70d6c4b1fd4d0
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-29 11:25:07 -0300

    Update funcionarios.php

--- funcionarios.php
+++ funcionarios.php
@@ -10,6 +10,11 @@ $empresa = "SELECT codcli, cliente, fantasia FROM cadcliente order by cliente";
 
 $empresas = $conexao->query($empresa) or die($mysqli->error);
 
+$pesquisa = filter_input(INPUT_POST,'pesquisa');
+if(!isset($pesquisa)){$pesquisa = "";}
+$lista = "SELECT * from cadfunc where nome like '%{$pesquisa}%'"; 
+$con = $conexao->query($lista) or die($mysqli->error);
+
 
 include __DIR__ . '/views/funcionarioss.php';
 include __DIR__ . '/includes/adminfooter.php';
