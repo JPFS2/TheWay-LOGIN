d83dd2a9ab8a3d12c8c5fcbf719e67dbe8e4bd88
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-11-30 16:46:32 -0300

    Update editarf.php

--- editarf.php
+++ editarf.php
@@ -4,18 +4,16 @@ include('model/conexao.php');
 
 $nome = $_POST["nome"];
 $email = $_POST["email"];
-$telefone = $_POST["telefone"];
 $funcao = $_POST["funcao"];
 $senha = $_POST["senha"];
-$codempresa = $_POST["codempresa"];
-$responsavel = $_POST["responsavel"];
+$lista = ["codfunc"];
 
 
-$sql = "UPDATE cadfunc SET nome ='$nome', email='$email', tel = $telefone, funcao = $funcao, senha = $senha, codempresa = $codempresa, responsavel = $responsavel WHERE codfunc = $codfunc";
+$sql = "UPDATE cadfunc SET nome ='$nome', email='$email', funcao = $funcao, senha = $senha WHERE nome ='$nome', email='$email', funcao = $funcao, senha = $senha ";
 
-$update = mysql_query($sql);
+$update = mysqli_query($sql);
 if ($update) {
-    echo "Atualizado com sucesso!";
+    echo "Atualizado com sucesso!";             
 } else {
     echo "Erro durante a atualização. Verifique os dados!";
 }
