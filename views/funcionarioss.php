b38ed59df2992b9369462a0ed881234143b45705
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-12-01 11:29:47 -0300

    Update funcionarioss.php

--- views/funcionarioss.php
+++ views/funcionarioss.php
@@ -75,8 +75,17 @@
                     </div>
                 </div>
             </li>
-            <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
+            <div class="sidebar-heading">
+                Utilitários
+            </div>
+            <li class="nav-item active">
+                <a class="nav-link" href="uteis.php">
+                    <i class="fa fa-th-large"></i>
+                    <span>Úteis</span></a>
+            </li>
+            <!-- Divider -->
+            
 
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
