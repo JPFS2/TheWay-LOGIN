d827676d639d240807589c872e7fba37f9aae18b
Johnnathan Pacífico <johnnathanpacifico@gmail.com>
2022-12-05 13:24:48 -0300

    Update sistemas.php
    Co-Authored-By: Marlon Ferreira dos Santos <marlonfsantos@outlook.com.br>

--- views/sistemas.php
+++ views/sistemas.php
@@ -83,10 +83,6 @@
 
 
             <div class="row">
-
-
-
-
                 <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                     <div class="icon-box">
                         <div class="icon"><i class=""></i></div>
