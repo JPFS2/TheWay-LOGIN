711993cb3fafcead4064dd4e7f271052d84e561c
JPFS2 <johnnathanpacifico@gmail.com>
2022-12-29 15:23:38 -0300

    Update uteiss.php

--- views/uteiss.php
+++ views/uteiss.php
@@ -314,8 +314,7 @@ include('verifica_login.php');
                                 <div class='card-body'>
                                     <div class='row no-gutters align-items-center'>
                                         <div class='col mr-2'>
-                                            <div class='text-xs font-weight-bold text-primary text-uppercase mb-1'>
-                                                APP 1</div>
+                                            <div class='text-xs font-weight-bold text-primary text-uppercase mb-1'></div>
                                             <div class='h5 mb-0 font-weight-bold text-gray-800'><a
                                                     href='{$pasta}/{$arquivo}'>{$arquivo}</a></div>
                                         </div>
